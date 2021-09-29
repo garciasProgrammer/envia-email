148565111d191edff80f555f34f11cb61840875e
garciasProgrammer <garcias.programmer@gmail.com>
2021-09-29 12:25:44 -0400

    initial commit

---
+++ src/main/java/com/mgemail/enviaemail/EnviaemailApplication.java
@@ -0,0 +1,13 @@
+package com.mgemail.enviaemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviaemailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviaemailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgemail/enviaemail/EnviaemailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgemail.enviaemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviaemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
